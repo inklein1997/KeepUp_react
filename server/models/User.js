db78994b321176659cb379cf33ba67045671067c
Michael Klein <Michaelklein1997@gmail.com>
2022-03-27 18:36:31 -0500

    created User Model

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const {Schema, model} = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        require: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    projects: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Project'
+    }],
+    notes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Note'
+    }]
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema, "User")
+
+module.exports = User
