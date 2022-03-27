42867045e42e2fcb0ad7c342e9b12d56db40fbad
Michael Klein <Michaelklein1997@gmail.com>
2022-03-27 18:20:30 -0500

    created initial server file

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const dbConnection = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+dbConnection.once("open", () => {
+  app.listen(PORT, () => {
+    console.log("API running on port ${PORT}");
+  });
+});
