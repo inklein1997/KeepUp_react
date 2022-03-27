1e57b660e0b0a9366dc185a1fff424b87e859cf9
Michael Klein <Michaelklein1997@gmail.com>
2022-03-27 18:48:44 -0500

    exporting all Models from models folder

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+const Project = require("./Project");
+const List = require("./List");
+const Task = require("./Task");
+const Note = require("./Note");
+
+module.exports = { User, Project, List, Task, Note };
