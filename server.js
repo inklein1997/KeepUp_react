2529735d5cbd7c26c2bf0d5d53688faf7a594059
Michael Klein <Michaelklein1997@gmail.com>
2022-03-28 16:22:40 -0500

    restructured repo to match MVC paradigm

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../views/build")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../views/build/index.html"));
 });
 
 sequelize.sync({ force: false }).then(() => {
